0d5b602a8b0a59a5b74b9c5e5ebf32f6bc6d1e99
GhituA <isthatpawz@gmail.com>
2020-08-26 08:46:32 +0300

    HTML/CSS part of the project

---
+++ source/js/main-nav.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  var navList = document.querySelector(".main-nav");
+  var navBtn = document.querySelector(".main-nav__toggle");
+  var siteList = document.querySelector(".site-list");
+
+  var setInactiveMode = function () {
+    navBtn.addEventListener("click", function () {
+      navList.classList.toggle("main-nav--js");
+      navBtn.classList.toggle("main-nav__toggle--open");
+      siteList.classList.toggle("site-list--js");
+    })
+
+    navList.classList.add("main-nav--js");
+    navBtn.classList.remove("main-nav__toggle--hidden");
+    navBtn.classList.add("page-header__toggle--open");
+  }
+
+  setInactiveMode();
+
+})();
